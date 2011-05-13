2d80a0a68fefd7469df68d241e7a97b445a9fbfd
orbisnull <zen4dev@gmail.com>
2011-05-14 02:44:08 +0900

    first page on django Hello World

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+from tdjango.views import hello
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,4 +15,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    ('^hello/$', hello),
 )
